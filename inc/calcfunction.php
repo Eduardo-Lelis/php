33d3ae168abde551501bc8df32c5a648a1ca4aa3
EduardoLelis <eduardodelelis@gmail.com>
2021-02-24 16:10:07 -0300

    My first function inside a php program

---
+++ inc/calcfunction.php
@@ -0,0 +1,12 @@
+<?php
+
+ /*Essa funcao sera responsavel pelo calculo de dois numero e apresentacao de resultado na tela
+*/
+
+function soma ($a,$b)
+{
+	return $a+$b;
+}
+
+
+?>
---
+++ returnnumber.php
@@ -0,0 +1,13 @@
+<?php
+
+include_once "inc/calcfunction.php";
+
+$resultado = soma (30,30);
+
+/* a variável acima está realizando o chamamento da funcão que se encontra na mesma pasta
+*/
+
+echo $resultado;
+
+
+?>
